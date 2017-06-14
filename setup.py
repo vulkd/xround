0b372bf0c0f4cad9516888d5217cdc29f1cb8e23
vulkd <vulkd@users.noreply.github.com>
2017-06-15 01:22:50 +1000

    fix version number

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name="xround",
-    version="0.1",
+    version="1.0",
     author="vulkd",
     author_email="vulkdmail@gmail.com",
     description="Rounded corners for X",
     url="https://github.com/vulkd/xround",
-    download_url ="https://github.com/vulkd/xround/archive/0.1.tar.gz",
+    download_url ="https://github.com/vulkd/xround/archive/1.0.tar.gz",
     scripts=["xround.py"],
     install_requires=["python-xlib"])
